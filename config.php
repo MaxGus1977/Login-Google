51fc1f5d29e55baec2302722c03032a78a3d3c14
MaxGus1977 <107018368+MaxGus1977@users.noreply.github.com>
2023-05-08 22:13:57 -0300

    Update config.php
    change client and token id by generic numbers

--- config.php
+++ config.php
@@ -11,11 +11,11 @@ require_once 'vendor/autoload.php';
 //Make object of Google API Client for call Google API
 $google_client = new Google_Client();
 
-//Set the OAuth 2.0 Client ID | Copiar "ID DE CLIENTE"
-$google_client->setClientId('919504303276-g22sf2f7ofb82nct4ollp95atgchj78f.apps.googleusercontent.com');
+//Set the OAuth 2.0 Client ID | Copiar "Aqui tu ID DE CLIENTE"
+$google_client->setClientId('919504303276-xxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com');
 
-//Set the OAuth 2.0 Client Secret key
-$google_client->setClientSecret('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');
+//Set the OAuth 2.0 Client Secret key Aqui tu CLAVE
+$google_client->setClientSecret('GOCSPX-xxxxxxxxxxxxxxxxxxxxxx');
 
 //Set the OAuth 2.0 Redirect URI | URL AUTORIZADO
 $google_client->setRedirectUri('http://localhost/DemoCrud4/index.php');
